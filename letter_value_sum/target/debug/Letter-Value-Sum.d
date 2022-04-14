9d5bd4230dd5d0faf0fa501f32a23591786ca259
AJ Cadell <acadell@AJs-MacBook-Pro.local>
2022-04-13 21:19:40 -0400

    Clean up binaries

--- letter_value_sum/target/debug/Letter-Value-Sum.d
+++
@@ -1,1 +0,0 @@
-/Users/acadell/Documents/Rust-Projects/letter_value_sum/target/debug/Letter-Value-Sum: /Users/acadell/Documents/Rust-Projects/letter_value_sum/src/main.rs
--- letter_value_sum/target/debug/deps/Letter_Value_Sum-00fa509227163c8b.d
+++
@@ -1,5 +0,0 @@
-/Users/acadell/Documents/Rust-Projects/letter_value_sum/target/debug/deps/Letter_Value_Sum-00fa509227163c8b: src/main.rs
-
-/Users/acadell/Documents/Rust-Projects/letter_value_sum/target/debug/deps/Letter_Value_Sum-00fa509227163c8b.d: src/main.rs
-
-src/main.rs:
